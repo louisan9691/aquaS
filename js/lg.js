6597d4f963c984dae4b4a0d0d3abf025cfb106b7
louisan9691 <louis.an9691@gmail.com>
2017-09-04 15:53:58 +1000

    Finished flavours.html with lightgallery.js library

---
+++ js/lg.js
@@ -0,0 +1,9 @@
+$(function() {
+  lightGallery(document.getElementById('animated-thumbnails'), {
+    thumbnail:true });
+
+    lightGallery(document.getElementById('captions'));
+
+    lightGallery(document.getElementById('hash'));
+
+});
