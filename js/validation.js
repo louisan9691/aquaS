920d357a346ae0c780a50d84f7df67dd2661cce1
louisan9691 <louis.an9691@gmail.com>
2017-09-06 02:23:12 +1000

    Minor

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,14 @@
 $(function() {
   // Initialize form validation on the registration form.
-  // It has the name attribute "registration"
   $("form").validate({
     // Specify validation rules
     rules: {
       // The key name on the left side is the name attribute
       // of an input field. Validation rules are defined
       // on the right side
-      name: "required",
+      name: {
+        required: true
+      },
       email: {
         required: true,
         // Specify that email should be validated
@@ -22,14 +23,22 @@ $(function() {
     },
     // Specify validation error messages
     messages: {
-      firstname: "Please enter your firstname",
-      email: "Please enter a valid email address",
+      name: {
+        required: "This is a required field."
+      },
+      email: {
+        required: "This is a required field.",
+        emai: "Please enter a valid email address."
+      },
       message: {
-        required: "Please provide a message",
-        minlength: "Your message must be at least 3 characters long",
-        maxlength: "Your message must be less than 100 characters long"
+        required: "This is a required field.",
+        minlength: "Your message must be at least 3 characters long.",
+        maxlength: "Your message must be less than 100 characters long."
       }
     },
+
+
+
     // Make sure the form is submitted to the destination defined
     // in the "action" attribute of the form when valid
     submitHandler: function(form) {
